c1b749d39608717b3a788b4f00e51dd99e3fb6ae
Pat114 <patricia.hogbom@student.nbi-handelsakademin.se>
2026-09-13 20:27:13 +0200

    Refactor test_hasttacke.py for cleaner formatting
    Removed unnecessary blank lines and ensured proper formatting.

--- tests/test_hasttacke.py
+++ tests/test_hasttacke.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.mark.unit2
 def test_vintertäcke():
     assert välj_täcke(2, False) == "vintertäcke"
-    
+
 #arrange - vintertäcke
 #act - kör 2 grader, regnar - false
 #assert - utvärdera
@@ -12,11 +12,7 @@ def test_vintertäcke():
 @pytest.mark.unit2
 def test_regntäcke():
     assert välj_täcke(10, True) == "regntäcke"
-    
+
 @pytest.mark.unit2    
 def test_inget_täcke():
     assert välj_täcke(15, False) == "inget täcke"
-
-
-
-

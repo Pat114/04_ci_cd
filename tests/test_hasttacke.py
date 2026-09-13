87ad8836a47f251483ebf93681a34ef46738f24c
Pat114 <patricia.hogbom@student.nbi-handelsakademin.se>
2026-09-13 20:50:18 +0200

    Fix formatting and cleanup in test_hasttacke.py

--- tests/test_hasttacke.py
+++ tests/test_hasttacke.py
@@ -6,13 +6,11 @@ import pytest
 def test_vintertäcke():
     assert välj_täcke(2, False) == "vintertäcke"
 
-#arrange - vintertäcke
-#act - kör 2 grader, regnar - false
-#assert - utvärdera
 
 @pytest.mark.unit2
 def test_regntäcke():
     assert välj_täcke(10, True) == "regntäcke"
+
 
 @pytest.mark.unit2    
 def test_inget_täcke():

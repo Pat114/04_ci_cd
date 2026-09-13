655734a5134fc317101d856c00079a5e08410b3e
Pat114 <patricia.hogbom@student.nbi-handelsakademin.se>
2026-09-13 20:57:29 +0200

    Fix formatting and add missing newline in test file

--- tests/test_hasttacke.py
+++ tests/test_hasttacke.py
@@ -12,6 +12,6 @@ def test_regntäcke():
     assert välj_täcke(10, True) == "regntäcke"
 
 
-@pytest.mark.unit2    
+@pytest.mark.unit2
 def test_inget_täcke():
     assert välj_täcke(15, False) == "inget täcke"
